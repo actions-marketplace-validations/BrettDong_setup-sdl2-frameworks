4a70e94209f9c2c7100cfe271446e9f15dc2a5b8
Binrui Dong <brett.browning.dong@gmail.com>
2023-03-23 19:02:43 +0800

    Retrieve latest SDL2 releases from GitHub API

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,106 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import * as io from '@actions/io'
 import * as tc from '@actions/tool-cache'
 import * as dmg from 'dmg'
 import path from 'path'
 import {promisify} from 'util'
+
 interface SDL2Component {
   name: string
   repo: string
   framework: string
-  dmg: string
 }
 
 const sdl2Components: SDL2Component[] = [
   {
     name: 'sdl2',
     repo: 'SDL',
-    framework: 'SDL2.framework',
-    dmg: 'SDL2'
+    framework: 'SDL2.framework'
   },
   {
     name: 'sdl2-ttf',
     repo: 'SDL_ttf',
-    framework: 'SDL2_ttf.framework',
-    dmg: 'SDL2_ttf'
+    framework: 'SDL2_ttf.framework'
   },
   {
     name: 'sdl2-image',
     repo: 'SDL_image',
-    framework: 'SDL2_image.framework',
-    dmg: 'SDL2_image'
+    framework: 'SDL2_image.framework'
   },
   {
     name: 'sdl2-mixer',
     repo: 'SDL_mixer',
-    framework: 'SDL2_mixer.framework',
-    dmg: 'SDL2_mixer'
+    framework: 'SDL2_mixer.framework'
   }
 ]
 
-function getDownloadURL(component: SDL2Component, version: string): string {
-  return `https://github.com/libsdl-org/${component.repo}/releases/download/release-${version}/${component.dmg}-${version}.dmg`
+interface Release {
+  version: string
+  url: string
+}
+
+interface Asset {
+  name: string
+  browser_download_url: string
+}
+
+function findDmgAsset(version: string, assets: Asset[]): Release {
+  for (const asset of assets) {
+    if (asset.name.includes('.dmg')) {
+      return {
+        version,
+        url: asset.browser_download_url
+      }
+    }
+  }
+  throw new Error(`Could not find a .dmg asset from the release ${version}`)
+}
+
+async function getLatestRelease(component: SDL2Component): Promise<Release> {
+  const octokit = github.getOctokit(core.getInput('token'))
+  const response = await octokit.rest.repos.getLatestRelease({
+    owner: 'libsdl-org',
+    repo: component.repo
+  })
+  const version = response.data.name
+  if (version === null) {
+    throw new Error('Got empty version name from the latest release')
+  }
+  return findDmgAsset(version, response.data.assets)
+}
+
+async function getSpecifiedRelease(
+  component: SDL2Component,
+  version: string
+): Promise<Release> {
+  const octokit = github.getOctokit(core.getInput('token'))
+  try {
+    const response = await octokit.rest.repos.getReleaseByTag({
+      owner: 'libsdl-org',
+      repo: component.repo,
+      tag: `release-${version}`
+    })
+    return findDmgAsset(version, response.data.assets)
+  } catch (err) {
+    if (err instanceof Error) {
+      if (err.message === 'Not Found') {
+        core.error(`Could not find ${component.name} version ${version}`)
+      }
+    }
+    throw err
+  }
+}
+
+async function getRelease(
+  component: SDL2Component,
+  version: string
+): Promise<Release> {
+  if (version === 'latest') {
+    return await getLatestRelease(component)
+  } else {
+    return await getSpecifiedRelease(component, version)
+  }
 }
 
 async function mountDmg(dmgPath: string): Promise<string> {
@@ -71,14 +132,15 @@ async function extractDmg(
 }
 
 async function install(component: SDL2Component): Promise<void> {
-  const version: string = core.getInput(component.name)
+  let version: string = core.getInput(component.name)
   if (version === 'none') {
     core.info(`Not installing ${component.name}`)
     return
   }
   const destination: string = core.getInput('dest')
-  core.info(`Installing ${component.name} ${version}`)
-  const url: string = getDownloadURL(component, version)
+  const release = await getRelease(component, version)
+  const url = release.url
+  version = release.version
   core.debug(`Downloading from ${url}`)
   const downloaded: string = await tc.downloadTool(url)
   core.debug(`Downloaded to ${downloaded}`)
@@ -86,6 +148,7 @@ async function install(component: SDL2Component): Promise<void> {
   await io.mv(downloaded, dmgPath)
   core.debug(`${downloaded} renamed to ${dmgPath}`)
   await extractDmg(dmgPath, component.framework, destination)
+  await io.rmRF(dmgPath)
   const finalPath = path.join(destination, component.framework)
   core.info(`Installed ${component.name} ${version} to ${finalPath}`)
 }
